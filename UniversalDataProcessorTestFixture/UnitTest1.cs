e837ca59c5205db8934fe74c0d44012b78d3d719
sunnylnct007 <sunnylnct007@gmail.com>
2023-02-15 14:12:07 +0000

    added test fixture

---
+++ UniversalDataProcessorTestFixture/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UniversalDataProcessorTestFixture
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
