1b4640a09b30194c5d4fef1c67851c3f6358a290
sunnylnct007 <sunnylnct007@gmail.com>
2023-02-16 16:55:42 +0000

    Test fix

--- src/UniversalDataProcessorTestFixture/ExtractFacadeTestFixture.cs
+++ src/UniversalDataProcessorTestFixture/ExtractFacadeTestFixture.cs
@@ -4,19 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversalDataProcessorService.Cache;
 using UniversalDataProcessorService.Extract;
 
 namespace UniversalDataProcessorTestFixture
 {
     [TestFixture]
-    public class ExtractFacadeTestFixture:TestBase
+    public class ExtractFactoryTestFixture:TestBase
     {
         [Test]
         public void Generates_File_Correctly()
         {
             var filePath = "..\\..\\..\\TestFile\\transactions.csv";
+            var cachefacade= serviceProvider.GetService<ICacheFacade>();
+            cachefacade.InitializeCache();
             var extractFacade = serviceProvider.GetService<IExtractFactory>();
-            extractFacade.GenerateExtract(filePath);
+            Assert.DoesNotThrow(() =>
+            {
+                extractFacade.GenerateExtract(filePath);
+            });
 
         }
     }
